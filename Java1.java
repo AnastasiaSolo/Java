039e2223dc0d8a951401f1a1af0d1f3da8a921a5
neacrizisicloud.com <neacrizis@gmail.com>
2022-08-09 14:38:57 +0300

    homew

---
+++ Java1.java
@@ -0,0 +1,11 @@
+class treal
+{
+    public static void main (String[] args)
+    {
+        int i,n=9,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)  {
+                System.out.print(i); }
+            System.out.print("\n");
+        }}}
